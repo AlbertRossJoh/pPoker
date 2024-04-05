fa40f8f18b8b02c5e0a605f4b2e9cc3be942892c
AlbertRossJoh <93578997+AlbertRossJoh@users.noreply.github.com>
2024-04-05 23:50:12 +0200

    docker compose setup, middleware needed for cors

--- p-poker-app/src/app/ApolloWrapper.tsx
+++ p-poker-app/src/app/ApolloWrapper.tsx
@@ -13,7 +13,7 @@ import {
 function makeClient() {
   const httpLink = new HttpLink({
     // this needs to be an absolute url, as relative urls cannot be used in SSR
-    uri: "http://localhost:8080/query",
+    uri: "http://backend:8080/query",
     // you can disable result caching here if you want to
     // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
     fetchOptions: { cache: "no-store" },
--- p-poker-backend/server.go
+++ p-poker-backend/server.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/AlbertRossJoh/planning-poker-backend/graph"
 )
 
 const defaultPort = "8080"
+
+func cors(ctx context.Context, next graphql.OperationHandler) graphql.ResponseHandler {
+	return func(ctx2 context.Context) *graphql.Response {
+		return nil
+	}
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -19,8 +27,8 @@ func main() {
 	}
 
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
-
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	// srv.AroundOperations(cors)
+	http.Handle("/", playground.Handler("GraphQL playground", "/graphql"))
 	http.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)

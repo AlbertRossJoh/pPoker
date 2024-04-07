a4eccb0988529f8189ff57cbf67ff3f920a18a4f
AlbertRossJoh <93578997+AlbertRossJoh@users.noreply.github.com>
2024-04-07 19:35:36 +0200

    Update page.tsx

--- src/app/game/[sessionid]/page.tsx
+++ src/app/game/[sessionid]/page.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { ApolloWrapper } from "../../ApolloWrapper";
 import GameDisplay from "@/app/components/gameDisplay";
 
-export default function Session() {
+
+export default function Page() {
   return (
     <React.StrictMode>
       <ApolloWrapper>
@@ -13,3 +14,4 @@ export default function Session() {
     </React.StrictMode>
   );
 }
+export const dynamicParams = false

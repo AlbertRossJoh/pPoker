103df572bc7008cbdf4d31b36fc5529e1ef0193b
AlbertRossJoh <albertrossj@gmail.com>
2024-04-08 12:36:12 +0200

    Minor cleanup

--- src/app/game/[sessionid]/page.tsx
+++ src/app/game/[sessionid]/page.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { ApolloWrapper } from "../../ApolloWrapper";
 import GameDisplay from "@/app/components/gameDisplay";
-import { useCookies } from "react-cookie";
-import MyDialog from "@/app/components/MyDialog";
 
 
 export default function Page() {
